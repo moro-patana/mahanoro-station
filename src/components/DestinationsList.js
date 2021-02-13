ba6adb74e8a251743d1692bbfc9109d7ba5f07a4
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-02-13 10:01:21 +0300

    map through the destinations

--- src/components/DestinationsList.js
+++ src/components/DestinationsList.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function DestinationsList({trips}) {
+export default function DestinationsList({trips, destinations}) {
     return (
         <div>
             <h2>🚍Where are you going?</h2>
+            {destinations.map((destination) => (
+                <div>
+                    <span>{destination}</span>
+                </div>
+            ))}
         </div>
     )
 }
