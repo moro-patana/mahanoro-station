9debb345ab9d0144b3c49252a90a2f18ec1b525c
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-02-13 14:20:04 +0300

    filter the trips which has the same name as the params

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,18 +1,37 @@
 import React, {useEffect} from 'react'
+import { useParams, Link } from 'react-router-dom';
 export default function Destination({trips, getTripsList}) {
+    const { destinationName } = useParams();
     console.log(trips);
     useEffect(() => {
         getTripsList()
       }, [])
       
+      function createTripTemplate(trip) {
+		return (
+			<Link to={`/destination/${trip.id}`} key={trip.id}>
+				<div>
+					<h4>{new Date(trip.departureTime).toDateString()}</h4>
+                    {trip.seats.map(seat => (
+                        <p>{seat.isAvailable}</p>
+                    ))}
+				</div>
+			</Link>
+		);
+        }
+        function showTripsFilteredByDestination() {
+            const filteredTrips = trips
+                .filter((trip) => trip.destination === destinationName)
+                .map(createTripTemplate);
+            if (filteredTrips.length === 0) {
+                return <p>No results.</p>;
+            }
+            return filteredTrips;
+        }
     return (
         <>
         <h2>Next Trip to:</h2>
-            {trips.map(trip => (
-                <div>
-                    <p>{trip.destination}</p>
-                </div>
-            ))}
+            <div>{showTripsFilteredByDestination()}</div>
         </>
     )
 }
