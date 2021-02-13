011bb3758592f19f202c94d9f07d5621bdd0c3d5
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-02-13 10:35:35 +0300

    updated the name of the date

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 export function getTripsList() {
     return async (dispatch) => {
       const res = await fetch(`https://gist.githubusercontent.com/Pinois/36bb5fbf9b6a686f0baf4006dd137bca/raw/a40d8b3f696a75f388db286d57bdd05a925fa0e7/trips.json`);
-      const users = await res.json();
+      const trips = await res.json();
       dispatch({
         type: "GET_Trips",
         value: trips
