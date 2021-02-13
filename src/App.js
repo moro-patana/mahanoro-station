42a7dfd9cd9708734e7f4e540504012b0f2b57a2
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-02-13 11:26:02 +0300

    used Route and imported the header menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
 import DestinationsList from "./components/DestinationsList"
 import Destination from "./containers/Destination"
+import HeaderMenu from "./components/HeaderMenu"
 
 export default function App() {
     return (
         <div>
             <h1>Hello</h1>
-            <DestinationsList/>
+            <HeaderMenu/>
+            <Switch>
+                <Route exact path="/"><DestinationsList/></Route>
+            </Switch>
             <Destination/>
         </div>
     )
