dca3fa259f62537542b3a611ee25d21d0124c0e6
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-02-13 10:34:54 +0300

    imported the destination

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import DestinationsList from "./components/DestinationsList"
+import Destination from "./components/Destination"
 
 export default function App() {
     return (
         <div>
             <h1>Hello</h1>
             <DestinationsList/>
+            <Destination/>
         </div>
     )
 }
