096ca37da5428f16c1a11ed637dc12c18a093137
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-02-13 10:45:59 +0300

    checked spelling mistake

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -33382,8 +33382,9 @@ function getTripsList() {
   return async dispatch => {
     const res = await fetch(`https://gist.githubusercontent.com/Pinois/36bb5fbf9b6a686f0baf4006dd137bca/raw/a40d8b3f696a75f388db286d57bdd05a925fa0e7/trips.json`);
     const trips = await res.json();
+    console.log(trips);
     dispatch({
-      type: "GET_Trips",
+      type: "GET_TRIPS",
       value: trips
     });
   };
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,9 @@ export function getTripsList() {
     return async (dispatch) => {
       const res = await fetch(`https://gist.githubusercontent.com/Pinois/36bb5fbf9b6a686f0baf4006dd137bca/raw/a40d8b3f696a75f388db286d57bdd05a925fa0e7/trips.json`);
       const trips = await res.json();
+      console.log(trips);
       dispatch({
-        type: "GET_Trips",
+        type: "GET_TRIPS",
         value: trips
       })
     }
