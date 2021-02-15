c9a385fdf3b398da328f6ba2006e57a82f2cfde5
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-02-15 11:04:26 +0300

    renamed a function

--- src/components/TripsDetails.js
+++ src/components/TripsDetails.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 import { useParams, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-export default function DestinationDetails() {
+export default function TripDetails() {
 	const { tripId } = useParams();
 
 	const trips = useSelector((state) => state.trips);
