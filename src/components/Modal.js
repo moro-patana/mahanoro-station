bb384f87d53de4515be79f7afa8a1519d35700ca
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-02-15 11:38:41 +0300

    set the modal

---
+++ src/components/Modal.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
+import ModalPage from "react-modal"
+
+export default function Modal({toggleModal, displayModal}) {
+    const trips = useSelector(state => state.trips)
+    return (
+        <ModalPage
+        isOpen={displayModal}
+        >
+            <button onClick={()=>{if(toggleModal) {toggleModal(false) }}}>close</button>
+            <h4>Booking confirmed</h4>
+            <p>
+            Thank you for trusting our services. 
+            Your booking has been added to your account. You can review it there.
+            </p>
+            <Link to={`/account`}>
+                <button>Check your account</button>
+            </Link>
+        </ModalPage>
+    )
+}
