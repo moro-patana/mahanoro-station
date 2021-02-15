1e6ed5e5aea76f06eed9dde345982e54aadc2d97
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-02-15 07:05:32 +0300

    created the trips details component

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -36568,7 +36568,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57328" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57787" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
