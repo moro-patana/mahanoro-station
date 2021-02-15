128de649f136a89b725c905c4e03b60084230cf0
Manjaka Randrianirina <manjaka.ran@onja.org>
2021-02-15 07:11:46 +0300

    set up the details

---
+++ src/components/TripsDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {useSelector} from "react-redux"
+
+export default function TripsDetails() {
+    const trips = useSelector(state => state.trips)
+    return (
+        <div>
+            <h2>Book a seat to:</h2>
+            {trips.map(trip => (
+                <div>
+                    <ul>
+                        {trip.seats.map(seat => (
+                            <li>{seat}</li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    )
+}
